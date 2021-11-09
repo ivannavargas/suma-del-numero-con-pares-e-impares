cfd3f7e0dce4a653a22d5e6d9919a48dda8f705e
Ivanna vargas <65833953+ivannavargas@users.noreply.github.com>
2021-11-08 23:23:29 -0500

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,21 @@
+let miarray = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
+
+miarray.forEach(function(a,b){
+
+  document.write(a+'&nbsp;'+"+"+b+"="+(a+b)+'&nbsp;'+"<br>");
+ 
+ 
+
+
+});
+
+
+
+ 
+
+
+
+
+
+
+
